8b1bc49040e8c8b1626f38025e2e8458c33d0e9d
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-13 16:31:07 +0530

    meals summary

---
+++ MealsSummary.js
@@ -0,0 +1,16 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food Delivered To Your</h2>
+      <p>
+        "Discover a world of culinary delights with our Food App. Explore a
+        diverse menu, order with ease, and savor delicious dishes delivered to
+        your doorstep!"
+      </p>
+      
+    </section>
+  );
+};
+export default MealsSummary;
